7377dbee091a9ddf8756c0610090e818615db6c8
juliandunne1234 <julianmorandunne@gmail.com>
2023-03-03 18:45:08 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'name',
+        'description',
+        'image',
+        'category',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Category(models.Model):
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=54)
     friendly_name = models.CharField(max_length=54, null=True, blank=True)
 
